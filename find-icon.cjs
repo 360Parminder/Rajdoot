38be426711b71114f3df46518983a3971f79aca4
parminder singh <yesask8@gmail.com>
2026-08-04 00:11:18 +0530

    removed test js files

--- find-icon.cjs
+++
@@ -1,3 +0,0 @@
-const icons = require('hugeicons-react');
-const names = Object.keys(icons).filter(name => name.toLowerCase().includes('chart') || name.toLowerCase().includes('bar'));
-console.log(names);
